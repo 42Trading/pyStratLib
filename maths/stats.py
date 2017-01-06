01d033ce3946278bcc3010639af5a5d78b56cecd
iLampard <siqiao_xue@163.com>
2017-01-06 08:57:29 +0800

    Clean the code of running_sum

--- maths/stats.py
+++ maths/stats.py
@@ -5,9 +5,9 @@ import itertools
 
 def running_sum(s, n):
     """
-    :param s:
-    :param n:
-    :return:
+    :param s: list of elements
+    :param n: length of running sum
+    :return: generator object of a running sum
     Generate the series of running sums of n elements of s.
 
     list(running_sum([1, 2, 3, 4], 2))
@@ -23,3 +23,7 @@ def running_sum(s, n):
     while True:
         yield rs
         rs += hi() - lo()
+
+
+if __name__ == "__main__":
+    print list(running_sum([1, 2, 3, 4], 3))
