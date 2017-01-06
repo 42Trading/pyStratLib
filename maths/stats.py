7ee76dbbdb31a18f5df2fef3b4c405c2a5cd7c4b
RoxanneYang <yanzheyangchn@outlook.com>
2017-01-06 09:41:46 +0800

    Rename function runningSum()

--- maths/stats.py
+++ maths/stats.py
@@ -3,19 +3,19 @@
 import itertools
 
 
-def running_sum(s, n):
+def runningSum(s, n):
     """
     :param s: list of elements
     :param n: length of running sum
     :return: generator object of a running sum
     Generate the series of running sums of n elements of s.
 
-    list(running_sum([1, 2, 3, 4], 2))
+    list(runningSum([1, 2, 3, 4], 2))
     [3, 5, 7]
-    rs = running_sum(itertools.count(), 3)
+    rs = runningSum(itertools.count(), 3)
     rs.next(), rs.next(), rs.next()
     (3, 6, 9)
-    list(running_sum([1, 2], 3))
+    list(runningSum([1, 2], 3))
     []
     """
     lo, hi = [i.next for i in itertools.tee(s)]
@@ -26,4 +26,4 @@ def running_sum(s, n):
 
 
 if __name__ == "__main__":
-    print list(running_sum([1, 2, 3, 4], 3))
+    print list(runningSum([1, 2, 3, 4], 3))
