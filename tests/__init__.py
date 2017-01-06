c3d2ead95bb4ea3e85b1e5ccc7f1be2f8a1f2192
RoxanneYang <yanzheyangchn@outlook.com>
2017-01-06 14:43:31 +0800

    add init files for unit-test

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from pyStratLib.tests import analyzer
+from pyStratLib.tests import maths
+from pyStratLib.tests import utils
+
+__all__ = ["analyzer", "maths", "utils"]
+
+__version__ = "0.0.1"
