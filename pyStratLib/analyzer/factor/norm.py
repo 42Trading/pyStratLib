d798125e2795b49fbd872a27f18601c6a1d5d611
iLampard <siqiao_xue@163.com>
2017-01-10 14:03:35 +0800

    Add an industry neutral function

--- pyStratLib/analyzer/factor/norm.py
+++ pyStratLib/analyzer/factor/norm.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 # ref: https://uqer.io/community/share/55ff6ce9f9f06c597265ef04
+import pandas as pd
+import numpy as np
+from PyFin.Utilities import pyFinAssert
+from sklearn.linear_model import LinearRegression
 
+
+
+
+_industryDict = {
+#TODO fill out the dict
+
+}
 
 def winsorize(factors, nb_std_or_quantile=3):
     """
@@ -37,14 +48,50 @@ def standardize(factors):
     return ret
 
 
-def neutralize(factors, industryTable, capTable=None):
+
+def getIndustryMatrix(industry):
+    """
+    :param industry: pd.Series, index = secID, value = 行业名称
+    :return: numpy.matrix, 行业虚拟矩阵，see alphaNote
+    """
+    secIDs = industry.index.tolist()
+    nbSecId = len(secIDs)
+    uniqueIndustry = industry.unique()
+    nbUnqiueIndustry = len(uniqueIndustry)
+    ret = np.zeros((nbSecId, nbUnqiueIndustry))
+    for i in range(len(secIDs)):
+        colIndex = np.where(uniqueIndustry == industry[i])[0]
+        ret[i][colIndex] = 1.0
+
+    return ret
+
+
+def neutralize(factors, industry, cap=None):
     """
     :param factors: pd.Series, 原始截面因子
-    :param industryTable: dict, key = secID, value = 行业名称
-    :param capTable: optional, dict, key = secID, value = cap value
+    :param industry: pd.Series, value = 行业名称
+    :param cap: optional, pd.Series, value = cap value
     :return: 中性化后的因子
     """
+    pyFinAssert(factors.size == industry.size, ValueError,
+                "factor size {0} does not equal to industry size {1}".format(factors.size, industry.size))
+    linreg = LinearRegression(fit_intercept=False)
+    if cap is None:
+        X = getIndustryMatrix(industry)
+        Y = factors
+        model = linreg.fit(X, Y)
 
-    factors = factors.copy()
+    else:
+        pyFinAssert(industry.size == cap.size, ValueError,
+                "industry size {0} does not equal to cap size {1}".format(industry.size, cap.size))
+        lcap = np.log(cap)
+        #TODO fill out this section
 
-    return
+    ret = pd.Series(linreg.residues_, index=factors.index)
+    return ret
+
+
+if __name__ == "__main__":
+    factors = pd.Series([5.0,5.0,10.0], index=['000001.SZ','000002.SZ','000003.SZ'])
+    industry = pd.Series(['801190.SI', '801190.SI','801200.SI'], index=['000001.SZ','000002.SZ','000003.SZ'])
+    print neutralize(factors, industry)
