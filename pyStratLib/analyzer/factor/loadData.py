8df6a8c23d0db51f8337b9a375a563872f19f25d
iLampard <siqiao_xue@163.com>
2017-01-11 08:51:50 +0800

    Remove some csv files and update the csv file path

--- pyStratLib/analyzer/factor/loadData.py
+++ pyStratLib/analyzer/factor/loadData.py
@@ -6,18 +6,10 @@ from pyStratLib.utils.dateutils import getPosAdjDate
 
 
 _factorPathDict = {
-    'BPS': ['..//..//data//factor//BPS.csv', 'q'],  # 每股净资产,季度频率,
-    'OPR': ['..//..//data//factor//OperRev.csv', 'q'],  # 营业利润,季度频率
-    'PRTYOY': ['..//..//data//factor//ProfitYoY.csv', 'q'],  # 净利润同比增长率,季度频率
-    'PRITTM': ['..//..//data//factor//NetProfitTTM.csv', 'q'],  # 净利润TTM,季度频率
-    'TRN': ['..//..//data//factor//Turnover.csv', 'm'],  # 月度换手率,月度频率
-    'ROEYOY': ['..//..//data//factor//RoeYoY.csv', 'q'],  # 净利润增长率（季度同比）,季度频率
-    'NAV': ['..//..//data//factor//NetAsset.csv', 'm'],  # 当日净资产,月度频率
-    'ROE': ['..//..//data//factor//ROE.csv', 'q'],  # 净资产收益率,季度频率
-    'PE': ['..//..//data//factor//PE_TTM.csv', 'm'],  # 市盈率,月度频率
-    'GM': ['..//..//data//factor//GrossMargin.csv', 'q'],  # 销售毛利润TTM,季度频率
-    'TTM': ['..//..//data//factor//TTM.csv', 'q'],  # 销售毛利率,季度频率
-    'CAP': ['..//..//data//factor//MktCap.csv', 'm'],  # 总市值,月度频率
+    'PRTYOY': ['..//..//data//factor//ProfitYoY.csv', 'q'],  # 净利润同比增长率,季度频率 - alpha因子
+    'TRN': ['..//..//data//factor//Turnover.csv', 'm'],  # 月度换手率,月度频率 - alpha因子
+    'ROE': ['..//..//data//factor//ROE.csv', 'q'],  # 净资产收益率,季度频率 -- 分层因子
+    'CAP': ['..//..//data//factor//MktCap.csv', 'm'],  # 总市值,月度频率 -- 分层因子
     'RETURN': ['..//..//data//return//monthlyReturn.csv', 'm']  # 收益,月度频率
 }
 
