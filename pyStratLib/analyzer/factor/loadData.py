c81914f58e177516ece7b8ca8573ee23886e9210
RoxanneYang <yanzheyangchn@outlook.com>
2017-01-12 16:19:01 +0800

    Add 'EP2TTM' 'GP2ASSET' 'SPTTM' 'PEG' csv file import paths

--- pyStratLib/analyzer/factor/loadData.py
+++ pyStratLib/analyzer/factor/loadData.py
@@ -2,22 +2,25 @@
 import os
 import zipfile
 import pandas as pd
-from pyStratLib.analyzer.factor.cleanData import getUniverseSingleFactor
 from pyStratLib.analyzer.factor.cleanData import adjustFactorDate
+from pyStratLib.analyzer.factor.cleanData import getUniverseSingleFactor
 from pyStratLib.utils.dateutils import getPosAdjDate
-
 
 _factorPathDict = {
     'PRTYOY': ['..//..//data//factor//ProfitYoY.csv', 'q'],  # 净利润同比增长率, 季度频率 - alpha因子
     'TRN': ['..//..//data//factor//Turnover.csv', 'm'],  # 月度换手率, 月度频率 - alpha因子
-    'TTM':['..//..//data//factor//NetProfitTTM.csv', 'q'],  # 最近12个月净利润, 季度频率 -- alpha因子
-    'BP': ['..//..//data//factor//BP_LF.csv', 'q'], # 净资产除以总市值, 季度频率 -- 分层因子
+    'TTM': ['..//..//data//factor//NetProfitTTM.csv', 'q'],  # 最近12个月净利润, 季度频率 -- alpha因子
+    'BP': ['..//..//data//factor//BP_LF.csv', 'q'],  # 净资产除以总市值, 季度频率 -- 分层因子
     'ROE': ['..//..//data//factor//ROE.csv', 'q'],  # 净资产收益率, 季度频率 -- 分层因子
     'CAP': ['..//..//data//factor//MktCap.csv', 'm'],  # 总市值, 月度频率 -- 分层因子
-    'EQTYGROTHYOY':['..//..//data//factor//ProfitYoY.csv', 'q'],  # 净资产同比增长率, 季度频率 -- 分层因子
+    'EQTYGROTHYOY': ['..//..//data//factor//ProfitYoY.csv', 'q'],  # 净资产同比增长率, 季度频率 -- 分层因子
     'ROEYOY': ['..//..//data//factor//RoeYoY.csv', 'q'],  # ROE同比增长率, 季度频率 -- alpha测试因子
     'RETURN': ['..//..//data//return//monthlyReturn.csv', 'm'],  # 收益,月度频率
-    'INDUSTRY':['..//..//data//industry//codeSW.csv', 'm']  # 申万行业分类,月度频率
+    'INDUSTRY': ['..//..//data//industry//codeSW.csv', 'm'],  # 申万行业分类,月度频率
+    'EP2_TTM': ['..//..//data//factor//EP2_TTM.csv', 'q'],  # 剔除非经常性损益的过去12 个月净利润/总市值, 季度频率 -- alpha测试因子
+    'GP2Asset': ['..//..//data//factor//GP2Asset.csv', 'q'],  # 销售毛利润/总资产, 季度频率 -- alpha测试因子
+    'SP_TTM': ['..//..//data//factor//SP_TTM.csv', 'q'],  # 过去12 个月总营业收入/总市值, 季度频率 -- alpha测试因子
+    'PEG': ['..//..//data//factor//PEG.csv', 'm'],  # 过去12 个月总营业收入/总市值, 月度频率 -- alpha测试因子
 }
 
 
@@ -29,11 +32,12 @@ def getDataDiv(saveCSVPath, numerator='NAV', denominator='CAP', freq='m'):
     :param freq: str, optional, the frequency of the data
     :return: DataFrame, the divide result
     """
+
     def getNewFactorSeries(data, freq):
         ret = adjustFactorDate(data,
-                                          data.index.levels[0][0],
-                                          data.index.levels[0][-1],
-                                          freq)
+                               data.index.levels[0][0],
+                               data.index.levels[0][-1],
+                               freq)
         ret.index.names = ['tradeDate', 'secID']
         return ret
 
@@ -66,7 +70,7 @@ class FactorLoader(object):
         self.__zipPath = zipPath
         # 由于因子csv文件较大,所以默认存储为压缩格式的文件, 第一次使用时自动解压缩
         self._unzipCsvFiles(self.__zipPath)
-        
+
     def _unzipCsvFiles(self, zipPath):
         """
         :param zipPath: str, 因子数据压缩包路径
@@ -117,4 +121,4 @@ if __name__ == "__main__":
     factor = FactorLoader('2015-01-05', '2015-12-30', ['NAV', 'ROE', 'RETURN'])
     # ret = factor.getFactorData()
     # print ret['RETURN']
-    #print getDataDiv('BP.csv', 'NAV', 'CAP')
+    # print getDataDiv('BP.csv', 'NAV', 'CAP')
